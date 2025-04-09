318322d70edd1bf3e7a7c30ae33c071897def0c7
Abdelilah04116 <abdelilahourti@gmail.com>
2025-04-09 14:44:48 +0100

    pour familiariser avec la methode de upload file

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,38 +1,59 @@
-from fastapi import FastAPI,APIRouter,Depends,UploadFile,status
-import os
-from models import ResponseSignal
-from helpers.config import get_settings,Settings
-from controllers import datacontroler,ProjectController
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
+import os
+from helpers.config import get_settings, Settings
+from controllers import DataController, ProjectController
 import aiofiles
+from models import ResponseSignal
+import logging
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
-    prefix='/api/v1/data',
-    tags=['api_v1',"data"],
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
 )
 
 @data_router.post("/upload/{project_id}")
-async def upload_data(project_id:str,file:UploadFile,app_settings: Settings = Depends(get_settings)):
-    #validation des fichiers
-    is_valid = datacontroler().validation_upload(file=file)
-    if not is_valid:
+async def upload_data(project_id: str, file: UploadFile,
+                      app_settings: Settings = Depends(get_settings)):
+
+    try:
+        data_controller = DataController()
+
+        is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+
+        if not is_valid:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal": result_signal
+                }
+            )
+
+        project_dir_path = ProjectController().get_project_path(project_id=project_id)
+        file_path, file_id = data_controller.generate_unique_filepath(
+            orig_file_name=file.filename,
+            project_id=project_id
+        )
+
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+
         return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                "signal":result_signal
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id": file_id
             }
         )
-    project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(
-        project_dir_path,
-        file.filename
-    )
-    async with aiofiles.open(file_path,"wb")as f:
-        while chunk :=await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
 
-    return JSONResponse(
+    except Exception as e:
+        logger.exception("Exception during file upload")
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={
-                "signal":ResponseSignal.FILE_UPLOAD_FAILED.value
+                "signal": "INTERNAL_SERVER_ERROR",
+                "error": str(e)
             }
-        )      
+        )
